6c812659d185aae04ade57fc67eb7fba8929d03a
Winston Pemberton <winstonpemberton@gmail.com>
2020-10-01 23:17:30 +0000

    Automatically backed up by Learn

--- app/models/author.rb
+++ app/models/author.rb
@@ -1,5 +1,9 @@
 class Author < ActiveRecord::Base
   validates :name, presence: true
   validates :email, uniqueness: true
+<<<<<<< HEAD
   validates :phone_number, length: {is: 10}
+=======
+  validates :content, length: {10}
+>>>>>>> eb4246c2d298024ca4fe682dce66367cf9a609b6
 end
